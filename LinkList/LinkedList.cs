4e570fa445b4468390f0becb6a1957bf8a1a13bd
unknown <bhotnikhil25@gmail.com>
2023-06-30 10:33:05 +0530

    [Nikhil]-Ability to create Linked List by appending 30 and 70 to 56
    - Node with data 56 is First Created
    - Next Append 30 to 56
    - Finally Append 70 to 30
    - LinkedList Sequence: 56->30->70

--- LinkList/LinkedList.cs
+++ LinkList/LinkedList.cs
@@ -45,5 +45,39 @@ namespace LinkList
                 current = current.Next;
             }
         }
+        public void InsertAtBeginning(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        /*
+         * Ability to create Linked
+                List by appending 30 and
+                70 to 56
+
+                - Node with data 56 is First Created
+                - Next Append 30 to 56
+                - Finally Append 70 to 30
+                - LinkedList Sequence: 56->30->70
+         */
+        public void Append(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
     }
 }
--- LinkList/Program.cs
+++ LinkList/Program.cs
@@ -18,8 +18,22 @@
                 - Finally 56 is added to 30
                 - LinkedList Sequence: 56->30->70
              */
-            linkedList.InsertAtBeginning(30);
-            linkedList.InsertAtBeginning(56);
+            //linkedList.InsertAtBeginning(30);
+            //linkedList.InsertAtBeginning(56);
+
+            /*
+             * Ability to create Linked
+                List by appending 30 and
+                70 to 56
+
+                - Node with data 56 is First Created
+                - Next Append 30 to 56
+                - Finally Append 70 to 30
+                - LinkedList Sequence: 56->30->70
+             */
+            linkedList.Add(56);
+            linkedList.Append(30);
+            linkedList.Append(70);
             linkedList.Display();
         }
     }
