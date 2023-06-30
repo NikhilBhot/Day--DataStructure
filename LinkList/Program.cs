cdcf448b54c98c44384822a33c000c29c0c3168b
unknown <bhotnikhil25@gmail.com>
2023-06-30 10:28:37 +0530

    [Nikhil]-Ability to create Linked List by adding 30 and 56 to 70
                    - Node with data 70 is First Created
                    - Next 30 is added to 70
                    - Finally 56 is added to 30
                    - LinkedList Sequence: 56->30->70

--- LinkList/Program.cs
+++ LinkList/Program.cs
@@ -9,10 +9,17 @@
              */
             LinkedList linkedList = new LinkedList();
 
-            linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
 
+            /*Ability to create Linked List by adding 30 and 56 to 70
+
+                - Node with data 70 is First Created
+                - Next 30 is added to 70
+                - Finally 56 is added to 30
+                - LinkedList Sequence: 56->30->70
+             */
+            linkedList.InsertAtBeginning(30);
+            linkedList.InsertAtBeginning(56);
             linkedList.Display();
         }
     }
